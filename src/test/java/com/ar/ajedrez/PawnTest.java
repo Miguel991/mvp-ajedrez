85b8c9e93c9b81bf69fd5a3132f1a01d24f7438c
Miguel991 <hectormiguelc300@gmail.com>
2020-05-20 23:41:10 -0300

    Crea la clase PawnTest

---
+++ src/test/java/com/ar/ajedrez/PawnTest.java
@@ -0,0 +1,53 @@
+package com.ar.ajedrez;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PawnTest {
+
+    private List<WrapperConfigBoard> configPosicionFicha;
+    private Board board;
+
+    @Before
+    public void setup(){
+
+        this.board = new Board();
+
+        this.configPosicionFicha = new ArrayList<>();
+        WrapperConfigBoard wrapperConfigBoard = new WrapperConfigBoard(Position.A2, new Pawn(Color.WHITE));
+        configPosicionFicha.add(wrapperConfigBoard);
+    }
+
+    @Test
+    public void moverUnPeonUnaCasilla(){
+
+        this.board.boardConfig(this.configPosicionFicha);
+        this.board.movePiece(Position.A2, Position.A3);
+
+        Piece pieceEsperada = this.board.getPiece(Position.A3);
+        Piece posicionVacia = this.board.getPiece(Position.A2);
+
+        assertThat(pieceEsperada.getName()).isEqualTo("P");
+        assertThat(posicionVacia).isNull();
+
+    }
+
+    @Test
+    public void moverUnPeonDosCasillas(){
+
+        this.board.boardConfig(this.configPosicionFicha);
+        this.board.movePiece(Position.A2, Position.A4);
+
+        Piece pieceEsperada = this.board.getPiece(Position.A4);
+        Piece posicionVacia = this.board.getPiece(Position.A2);
+
+        assertThat(pieceEsperada).describedAs("No Existe pieza en la posicion A4").isNotNull();
+        assertThat(pieceEsperada.getName()).isEqualTo("P");
+        assertThat(posicionVacia).isNull();
+    }
+}
